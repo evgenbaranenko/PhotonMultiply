2c4e51b7255504dfd68ddfa305584c625c4a61e6
evgenbaranenko <evgenbaranenko@gmail.com>
2022-11-19 19:57:26 +0200

    add working version game

--- Assets/Script/PlayerDog.cs
+++ Assets/Script/PlayerDog.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerDog : MonoBehaviour, IPunObservable
+public class PlayerDog : MonoBehaviour, IPunObservable // реализуем интерфейс для того что бы photon мог за ним наблюдать
 {
     [SerializeField] private PhotonView photonView;
 
@@ -21,6 +21,10 @@ public class PlayerDog : MonoBehaviour, IPunObservable
 
     [SerializeField] private GameObject nameTextObj;
 
+    private SpriteRenderer spriteRenderer;
+
+    private bool isRed;
+
     Vector3 smoothMove;
     private void Awake()
     {
@@ -31,28 +35,66 @@ public class PlayerDog : MonoBehaviour, IPunObservable
     {
         photonView = GetComponent<PhotonView>();
         //playerCamera.SetActive(photonView.IsMine);
-        nameText.text = photonView?.Owner.NickName; 
+        nameText.text = photonView?.Owner.NickName;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
+        PlayerRed();
+
+        ReverseMovePlayerInAxe_x();
+
+        if (!photonView.IsMine) return;
         ProcessInputs();
         MovePlayerInAxe_x();
-        ReverseMovePlayerInAxe_x();
-        if (photonView.IsMine) ProcessInputs();
     }
+
+
+
+
+    public void PlayerRed()
+    {
+        // пользовательский ввод 
+        if (photonView.IsMine)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                isRed = true;
+            }
+            else
+            {
+                isRed = false;
+            }
+        }
+
+        // отображение состояния на экране 
+        if (isRed)
+        {
+            spriteRenderer.color = Color.red;
+        }
+        else
+        {
+            spriteRenderer.color = Color.white;
+        }
+    }
+
+
     public void SetName(string name)
     {
         nameText.text = name;
     }
     private void ProcessInputs()
     {
+        // if (!photonView.IsMine) return;
         var move = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
         transform.position += move * moveSpeed * Time.deltaTime;
     }
+
     public void MovePlayerInAxe_x()
     {
-        if (!photonView.IsMine) return;
+        // if (!photonView.IsMine) return;
 
         Vector2 velocity = playerRigidbody.velocity;
 
@@ -64,26 +106,29 @@ public class PlayerDog : MonoBehaviour, IPunObservable
     }
     public void ReverseMovePlayerInAxe_x()
     {
-        if (!photonView.IsMine) return;
-
-        if (transform.localScale.x < 0)
+        
+        if (photonView.IsMine)
         {
-            if (Input.GetAxis("Horizontal") < 0)
+            if (transform.localScale.x < 0)
             {
-                transform.localScale = Vector3.one;
-            }
+                if (Input.GetAxis("Horizontal") < 0)
+                {
+                    transform.localScale = Vector3.one;
+                }
 
-            DirectionNameText(directionNameText = false);
-        }
-        else
-        {
-            if (Input.GetAxis("Horizontal") > 0)
+                directionNameText = false;
+            }
+            else
             {
-                transform.localScale = new Vector3(-1, 1, 1);
-            }
+                if (Input.GetAxis("Horizontal") > 0)
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
 
-            DirectionNameText(directionNameText = true);
+                directionNameText = true;
+            }
         }
+        DirectionNameText(directionNameText);
     }
     public void DirectionNameText(bool direction)
     {
@@ -96,17 +141,23 @@ public class PlayerDog : MonoBehaviour, IPunObservable
             nameTextObj.transform.localScale = new Vector3(0.01f, 0.01f, 1);
         }
     }
+    // тут пишем код который передает по сети все состояние объекта
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
         {
-            stream.SendNext(transform.position);
+            // stream.SendNext(transform.position);
+            stream.SendNext(isRed);
+            stream.SendNext(directionNameText);
         }
-        else if (stream.IsReading)
+        else
         {
-            smoothMove = (Vector3)stream.ReceiveNext();
+            // smoothMove = (Vector3)stream.ReceiveNext();
+            isRed = (bool)stream.ReceiveNext();
+            directionNameText = (bool)stream.ReceiveNext();
         }
     }
 }
 
-        
+
+
